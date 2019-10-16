cfb425d13f208e5b0e9c6b9a728a3e31f0e5b46b
imsahil007 <35963992+imsahil007@users.noreply.github.com>
2019-10-16 19:36:33 +0530

    Update creating file  11.py

--- creating file  11.py
+++ creating file  11.py
@@ -1,5 +1,5 @@
 name=input("Enter the name:- ")
 age=int(input("Enter the age:- "))
 
-print("The name of the cadinate is",name,"and age is",age)
+print("The name of the candidate is",name,"and age is",age)
 input("press enter to quit")
